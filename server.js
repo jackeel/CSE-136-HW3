764de75ed84143957fb881ce7358d9fb4453482a
Jack Lee <jal179@ucsd.edu>
2016-05-15 15:10:11 -0700

    Refactored login check and resolved conflicts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var config = require('./server/config/config');
 var db = require('./server/config/db');
 var bookmarks = require('./server/api/bookmarks.js');
 var users = require('./server/api/users.js');
-var reset = require('./server/api/emailReset.js');
+//var reset = require('./server/api/passwordReset.js');
 
 db.init();
 
@@ -44,7 +44,6 @@ app.use(function(req, res, next) {
     next();
 });
 
-
 //TODO: middleware to check if js is turned off and check user-agent.
 //if missing, log for suspicion. Store ip and differentiate from ajax requests
 
@@ -80,22 +79,34 @@ app.post('/login', users.login);
 //app.get('/logout', users.logout);
 app.get('/signup', users.signupForm);
 app.post('/signup', users.signup);
-app.get('/passwordReset', reset.passwordresetForm);
-app.post('/passwordReset', reset.passwordReset);
+//app.get('/passwordReset', reset.passwordresetForm);
+//app.post('/passwordReset', reset.passwordReset);
 
 /*  This must go between the users routes and the books routes */
 //app.use(users.auth);
 
-app.get('/list/:folder_id(\\d+)?', requireLogin ,bookmarks.listBookmarks, bookmarks.listFolders, bookmarks.list);
-app.get('/bookmarks/edit/:bookmark_id(\\d+)', requireLogin,bookmarks.edit);
-app.get('/bookmarks/delete/:bookmark_id(\\d+)', requireLogin,bookmarks.delete);
-//app.get('/books/confirmdelete/:book_id(\\d+)', books.confirmdelete);
-app.post('/bookmarks/update/:bookmark_id(\\d+)', requireLogin,bookmarks.update);
-app.post('/insert', requireLogin ,bookmarks.insert);
+/**
+ * Redirect to login page if user isn't logged in.
+ * Note: Place login route before this and any routes that require login after this.
+ */
+app.use(function(req, res, next) {
+    if (req.session.userId == null){
+        res.redirect('/login');
+    } else {
+        next();
+    }
+});
 
-app.get('/list/starred', requireLogin ,bookmarks.listStarred);
-app.get('/bookmarks/:bookmark_id(\\d+)/star', requireLogin,bookmarks.star);
-app.get('/bookmarks/:bookmark_id(\\d+)/unstar', requireLogin,bookmarks.unstar);
+app.get('/list/:folder_id(\\d+)?', bookmarks.listBookmarks, bookmarks.listFolders, bookmarks.list);
+app.get('/bookmarks/edit/:bookmark_id(\\d+)', bookmarks.edit);
+app.get('/bookmarks/delete/:bookmark_id(\\d+)',bookmarks.delete);
+//app.get('/books/confirmdelete/:book_id(\\d+)', books.confirmdelete);
+app.post('/bookmarks/update/:bookmark_id(\\d+)', bookmarks.update);
+app.post('/insert',bookmarks.insert);
+
+app.get('/list/starred', bookmarks.listStarred);
+app.get('/bookmarks/:bookmark_id(\\d+)/star', bookmarks.star);
+app.get('/bookmarks/:bookmark_id(\\d+)/unstar', bookmarks.unstar);
 
 // http://www.mcanerin.com/EN/search-engine/robots-txt.asp use to generate and
 // set trap if a disallowed endpoint is hit and log them.
@@ -124,20 +135,12 @@ function errorHandler(err, req, res, next) {
   res.render('error', { error: err });
 }
 
-//used to confirm user is logged in in combination to the middleware
-function requireLogin (req, res, next) {
-  if (!req.userId) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
-
 app.listen(config.PORT, function () {
   console.log('Bookmarx app listening on port ' + config.PORT + '!');
 });
 
 
+/* Redirect all 404 to 404.html */
 app.use(function(req, res, next){
   res.status(404);
   // respond with html page
